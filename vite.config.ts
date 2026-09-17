d1f9f6b99104a68e43fdccea232f6da9d721dce1
Lorenzo Zulli <lorenzo.zulli@futura-dev.com>
2026-09-17 17:05:22 +0200

    feat: react pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Task Manager',
+        short_name: 'Task Manager',
+        theme_color: '#ffcc5f',
+        icons: [
+          {
+            src: 'assets/manifest-icons/pwa-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'assets/manifest-icons/pwa-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      }
+    })
+  ]
 })
